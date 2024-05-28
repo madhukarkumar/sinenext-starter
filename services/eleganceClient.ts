0bd2a3ba613838695d8ec084199eb98ec6ec998a
Madhukar Kumar <madhukar.kumar@gmail.com>
2024-05-27 19:52:35 -0700

    Base URL
    Removed the hard coded API_BASE_URL

--- services/eleganceClient.ts
+++ services/eleganceClient.ts
@@ -1,5 +1,7 @@
 import { createEleganceClient } from "@singlestore/elegance-sdk";
 
-export const eleganceClient = createEleganceClient("mysql", {
-  baseURL: process.env.API_BASE_URL || 'http://localhost:3000/api',
-});
+const baseURL = process.env.NODE_ENV === "production"
+    ? "/api"  // Production environment
+    : "http://localhost:3000/api"; // Development environment
+
+export const eleganceClient = createEleganceClient("mysql", { baseURL });
